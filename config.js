6ba53270d1fb85c64c338e6031359fc0a24a8b81
Leandro De La Cruz <tallerestacruz@gmail.com>
2021-02-20 05:46:03 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	secretKey: "12345-67890-09876-54321",
 	mongoUrl: "mongodb://localhost:27017/nucampsite",
+	facebook: {
+		clientId: "540939450220436",
+		clientSecret: "5d376e7c9dd81a9c8b4ce352778eec31",
+	},
 };
